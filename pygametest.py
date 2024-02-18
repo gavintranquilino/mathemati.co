a5525cdec1d988e179a8ffe8eb055221b900fe91
Adam <134978231+sullynumber9@users.noreply.github.com>
2024-02-17 19:32:27 -0500

    8/9 pygametest working

--- pygametest.py
+++ pygametest.py
@@ -17,12 +17,43 @@ patrick = pygame.font.Font(None, 100)  # You can choose a font and size - the nu
 
 
 # Function to display a number as an object
-def display_number(x, y):
-    text = patrick.render(str(1), True, (0, 0, 0))  # Render the text
+def display_number(number, x, y):
+    text = patrick.render(str(number), True, (0, 0, 0))  # Render the text
     screen.blit(text, (x, y))  # Draw the text on the screen
+    
+def distances(mouse_x, mouse_y, x_positions, y_positions):
+    # return the smallest distance and edit those values
+    
+    # dist list is a list with all of the different distances from each number
+    dist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    for i in range(len(x_positions)):
+        
+        # calculate the distance for one of the individual values
+        
+        dist[i] = (pygame.math.Vector2(mouse_x - x_positions[i], mouse_y - y_positions[i]).length())
+        
+        print(i, dist[i])
+        
+    # find the lowest distance of the distances
+    
+    for i in range(len(dist) - 1):
+        if dist[i] < dist[i+1]:
+            lowest_index = i
+        else:
+            lowest_index = i+1
+    
+    return lowest_index
+    
 
 # Initial position of the number
-number_x, number_y = 100, 100
+    
+# number_x, number_y = 50, 50
+number_x = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+number_y = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+
+offset_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+offset_y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # Main game loop
 while True:
@@ -35,14 +66,23 @@ while True:
 
     # Get the position of the mouse cursor
     mouse_x, mouse_y = pygame.mouse.get_pos()
+    
+    # get the shortest distance away
+    change = distances(mouse_x, mouse_y, number_x, number_y)
 
     # Check if the mouse cursor is within the bounding box of the number
-    if number_x < mouse_x < number_x + 36 and number_y < mouse_y < number_y + 36:
+    
+    # needs to change to be the number you want - the one with the lowest distance
+    
+    # need to somehow code the distance to the lowest distance
+    
+    
+    if number_x[change] < mouse_x < number_x[change] + 36 and number_y[change] < mouse_y < number_y[change] + 36:
         # Check for mouse click events
         if pygame.mouse.get_pressed()[0]:  # Left mouse button
             dragging = True
-            offset_x = mouse_x - number_x
-            offset_y = mouse_y - number_y
+            offset_x[change] = mouse_x - number_x[change]
+            offset_y[change] = mouse_y - number_y[change]
 
     # Check for mouse release events
     if not pygame.mouse.get_pressed()[0]:
@@ -50,9 +90,18 @@ while True:
 
     # Update the position of the number if dragging
     if dragging:
-        number_x, number_y = mouse_x - offset_x, mouse_y - offset_y
+        number_x[change], number_y[change] = mouse_x - offset_x[change], mouse_y - offset_y[change]
 
     # Display the number at the updated position
-    display_number(number_x, number_y)
+    display_number(0, number_x[0], number_y[0])
+    display_number(1, number_x[1], number_y[1])
+    display_number(2, number_x[2], number_y[2])
+    display_number(3, number_x[3], number_y[3])
+    display_number(4, number_x[4], number_y[4])
+    display_number(5, number_x[5], number_y[5])
+    display_number(6, number_x[6], number_y[6])
+    display_number(7, number_x[7], number_y[7])
+    display_number(8, number_x[8], number_y[8])
+    display_number(9, number_x[9], number_y[9])
 
     pygame.display.flip()  # Update the display
---
+++ rangepy.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(i)
+    
+    
+number_x = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+
+for i in range(len(number_x)):
+    print(i)
