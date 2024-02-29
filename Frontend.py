08b254d9cdddef69bf61b8608577148852fe50c1
Patrick Gaied <patrickayman1@gmail.com>
2024-02-28 22:01:51 -0500

    i added a different function

--- Frontend.py
+++ Frontend.py
@@ -1,32 +1,9 @@
 # 1. pass them in (x coord, y cord, 999, value)
 import pygame
+
 # import sys
 
-
-# # NEW IMPORTANT CODE STARTS HERE ==================================================================================
-
 # # DEDICATED TO MIJIC!!!!!
-
-# import random
-
-# from Objects import Number
-# from Objects import Operations
-# from Objects import GarbageCan
-# from Objects import Squares
-# from Objects import Circle
-# from Objects import Equals
-# from Objects import Answer
-
-# 2. save them elsewhere
-
-# DEFINING ALL OF THE NUMBERS (X COORD, Y COORD, DISTANCE, VALUE OF THE NUMBER
-
-
-
-
-# NEW IMPORTANT CODE ENDS HERE ==================================================================================
-
-
 
 # PYGAME STUFF STARTS HERE ----------------------------------------------------------------------------
 
@@ -34,11 +11,13 @@ import pygame
 pygame.init()
 font = pygame.font.Font(None, 100)  # You can choose a font and size
 
+
 # Function to display a number as an object
 
 def display_number(number, x, y, screen):
     text = font.render(str(number), True, (0, 0, 0))  # Render the text
     screen.blit(text, (x, y))  # Draw the text on the screen
+
 
 def display_numbers(numbers, screen):
     for i in range(len(numbers)):
@@ -52,10 +31,13 @@ def display_numbers(numbers, screen):
 # screen.blit(scaled_garbage, (garbage.x, garbage.y)) # Display the garbage imaege to the screen
 # display the garbage can as a function
 def display_garbage(garbage, screen):
-    garbage_can = pygame.image.load(garbage.value) # Load the garbage image
-    garbage_rect = garbage_can.get_rect() # Create a rectangle for pygame
-    scaled_garbage = pygame.transform.scale(garbage_can, (125, 125)) # Scale the garbage image (only present here)
-    screen.blit(scaled_garbage, (garbage.x, garbage.y)) # Display the garbage imaege to the screen
+    garbage_can = pygame.image.load(garbage.value)  # Load the garbage image
+    garbage_rect = garbage_can.get_rect()  # Create a rectangle for pygame
+    scaled_garbage = pygame.transform.scale(garbage_can, (
+    125, 125))  # Scale the garbage image (only present here)
+    screen.blit(scaled_garbage, (
+    garbage.x, garbage.y))  # Display the garbage imaege to the screen
+
 
 # DISPLAYING THE FIRST GREEN BOX
 
@@ -63,6 +45,7 @@ def display_green_box(green_box, screen):
     green_box_display = pygame.image.load(green_box.value)
     green_box_rect = green_box_display.get_rect()
     screen.blit(green_box_display, (green_box.x, green_box.y))
+
 
 # # DISPLAYING THE SECOND GREEN BOX
 # green_box_two_display = pygame.image.load(green_box_two.value)
@@ -84,13 +67,14 @@ def display_equals(equals, screen):
     equals_rect = equals_display.get_rect()
     screen.blit(equals_display, (equals.x, equals.y))
 
+
 # # DISPLAYING THE ANSWER
 
 # display the value on pygame screen given a int value and screen
 def display_value(value, screen):
-    value_display = pygame.font.Font(None, 80).render("Score:" + str(value), True, (0, 0, 0))
+    value_display = pygame.font.Font(None, 80).render("Score:" + str(value),
+                                                      True, (0, 0, 0))
     screen.blit(value_display, (180, 360))
-
 
 
 # answer_display = font.render(str(answer.value), True, (0, 0, 0))
@@ -100,6 +84,7 @@ def display_answer(answer, screen):
     answer_display = font.render(str(answer.value), True, (0, 0, 0))
     screen.blit(answer_display, (answer.x, answer.y))
 
+
 # display the operation as a function
 def display_operation(operation, screen):
     operation_display = font.render(str(operation.value), True, (0, 0, 0))
@@ -107,10 +92,11 @@ def display_operation(operation, screen):
 
 
 # DISPLAYING THE OPERATIONS
-    
+
 def display_operations(operations, screen):
     for i in range(len(operations)):
-        operations_display = font.render(str(operations[i].value), True, (0, 0, 0))
+        operations_display = font.render(str(operations[i].value), True,
+                                         (0, 0, 0))
         screen.blit(operations_display, (operations[i].x, operations[i].y))
 # for i in range(len(operations)):
 #     display_number(operations[i].value, operations[i].x, operations[i].y)
